25c13505a410cd2fd21083f66519a514f859f91c
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-30 10:18:25 -0300

    chore: init

---
+++ src/main/java/com/example/workflowtest/WorkflowTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.workflowtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workflowtest/WorkflowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workflowtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
